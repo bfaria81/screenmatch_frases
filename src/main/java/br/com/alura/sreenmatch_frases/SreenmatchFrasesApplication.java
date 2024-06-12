49a58ea61f8f4c5b76080c4cd59ceedb619b3f8b
Bruno <bfaria81@hotmail.com>
2024-06-12 17:22:57 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/sreenmatch_frases/SreenmatchFrasesApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.sreenmatch_frases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreenmatchFrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreenmatchFrasesApplication.class, args);
+	}
+
+}
